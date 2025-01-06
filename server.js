9cc57a22e58ddb943e5957bf8b823fcbebc8bc48
fokjul <julia.fok86@gmail.com>
2025-01-06 09:19:33 -0800

    adds vercel cconfig

--- server.js
+++ server.js
@@ -19,16 +19,13 @@ const courseRoutes = require("./routes/course-routes");
 // Mount API routes under /
 app.use("/courses", courseRoutes);
 
-//When we start the server check if tables from the model folder exist in db and if don't - create them
-// Sync models with the database: ensure tables exist or create them if they don't
 db.sequelize
   .sync()
   .then(() => {
     console.log("Database synced successfully.");
   })
   .catch((error) => {
-    console.error("Error syncing the database:", error);
+    console.error("Error syncing the database", error);
   });
 
-// Export the app for Vercel
 module.exports = app;
