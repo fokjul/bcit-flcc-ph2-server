5d578b9562d662ab04da76c6037c121b8938b155
fokjul <julia.fok86@gmail.com>
2025-01-06 09:58:02 -0800

    updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({ origin: "*" }));
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-const db = require("./models");
+// const db = require("./models");
 
 // Import course routes
 const courseRoutes = require("./routes/course-routes");
@@ -19,13 +19,13 @@ const courseRoutes = require("./routes/course-routes");
 // Mount API routes under /
 app.use("/courses", courseRoutes);
 
-db.sequelize
-  .sync()
-  .then(() => {
-    console.log("Database synced successfully.");
-  })
-  .catch((error) => {
-    console.error("Error syncing the database", error);
-  });
+// db.sequelize
+//   .sync()
+//   .then(() => {
+//     console.log("Database synced successfully.");
+//   })
+//   .catch((error) => {
+//     console.error("Error syncing the database", error);
+// });
 
 module.exports = app;
